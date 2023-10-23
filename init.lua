a1b344284fbdcd0ee431a3d86783bd70c17834e5
Maharshi Joshi <joshi.maharshi.a@gmail.com>
2023-10-23 13:12:36 -0400

    Added options.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+for _, source in ipairs {
+    "core.options",
+} do 
+    local ok, fault = pcall(require, source)
+    if not ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
---
+++ lua/core/options.lua
@@ -0,0 +1,40 @@
+local options = {
+    opt = {
+        breakindent = true, -- Wrapped line matches indent
+        clipboard = "unnamedplus", -- Connects to system clipboard
+        cmdheight = 0, -- Don't show command line unless in use
+        completeopt = { "menu", "menuone", "noselect" }, -- Insert mode completion
+        copyindent = true, -- Copy indent of previous line
+        expandtab = true, -- Use spaces when a tab is inserted
+        fileencoding = "utf-8", -- Use Utf-8 file encoding
+        ignorecase = true, -- Ignore case in search patterns
+        infercase = true, -- Infers the case in keyword completion
+        laststatus = 3, -- Tells when last window has status line
+        mouse = "a", -- Enable mouse support
+        number = true, -- Show line numbers
+        preserveindent = true, -- Preserve indent structure when reindenting
+        relativenumber = true, -- Show relative line numbers
+        shiftwidth = 2, -- Number of spaces inserted for indentation
+        showtabline = 2, -- Always show tab line
+        signcolumn = "yes", -- Always show sign column
+        smartcase = true, -- Override ignore case if the search pattern has upper case characters
+        smartindent = true, -- Add indent when starting a new line
+        swapfile = false, -- Disable using swapfiles
+        tabstop = 2, -- Number of spaces that the tab key inserts
+        termguicolors = true, -- Enable colors for the terminal
+        undofile = true, -- Save undo history in a file
+        wrap = false, -- Do not wrap lines
+        writebackup = false, -- Disable making backup before overwriting a file
+    },
+    g = {
+        mapleader = " ", -- Set spacebar as leader key
+        maplocalleader = " ", -- Set default local leader key
+    },
+    t = vim.t.bufs and vim.t.bufs or { bufs = vim.api.nvim_list_bufs() }, -- Initialize buffers for current tab
+}
+
+for scope, table in pairs(options) do
+    for setting, value in pairs(table) do
+        vim[scope][setting] = value
+    end
+end
