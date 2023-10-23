4134a685320a49883eea0dc8c88497a17cde15b3
Maharshi Joshi <joshi.maharshi.a@gmail.com>
2023-10-23 19:58:57 -0400

    Added lazy as package manager

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 for _, source in ipairs {
     "core.options",
+    "core.lazy",
 } do 
     local ok, fault = pcall(require, source)
     if not ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
