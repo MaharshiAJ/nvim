142db7d8b9a44b661f4467c745bad1c480f34a32
Maharshi Joshi <joshi.maharshi.a@gmail.com>
2023-10-30 21:35:53 -0400

    Added none-ls for auto formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -46,5 +46,60 @@ return {
                 end,
             }
         end
+    },
+    {
+        "nvimtools/none-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "jay-babu/mason-null-ls.nvim" },
+        config = function()
+            local mason_null_ls = require("mason-null-ls")
+            local null_ls = require("null-ls")
+
+            mason_null_ls.setup({
+                ensure_installed = {
+                    "black",
+                    "clang-format",
+                    "djlint",
+                    "prettier",
+                    "pylint",
+                    "stylua",
+                }
+            })
+
+            local formatting = null_ls.builtins.formatting
+            local diagnostics = null_ls.builtins.diagnostics
+
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+            null_ls.setup({
+                sources = {
+                    formatting.black,
+                    formatting.clang_format,
+                    diagnostics.djlint,
+                    formatting.djlint,
+                    formatting.prettier,
+                    diagnostics.pylint,
+                    formatting.stylua,
+                },
+                on_attach = function(current_client, bufnr)
+                    if current_client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({
+                            filter = function(client)
+                                --  only use null-ls for formatting instead of lsp server
+                                return client.name == "null-ls"
+                            end,
+                            bufnr = bufnr,
+                            })
+                        end,
+                        })
+                    end
+                end,
+            })
+        end,
     }
 }
