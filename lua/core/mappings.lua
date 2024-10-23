82ee162773b22b1e13c0910b406cac955b493f57
MaharshiAJ <joshi.maharshi.a@gmail.com>
2024-10-23 10:43:35 -0400

    attempted adding custom snippet support

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,200 +1,58 @@
-local mapping_table = {
+local mappings = {
   n = {
     -- General
-    { "<Leader>w", "<cmd>w!<cr>",            desc = "Save File" },
-    { "<Leader>q", "<cmd>confirm q<cr>",     desc = "Close Buffer" },
-    -- Lazy
-    { "<Leader>p", group = "+Plugin Manager" },
-    {
-      "<Leader>pi",
+    ["<Leader>w"] = { "<cmd>w!<cr>", "Save File" },
+    ["<Leader>q"] = { "<cmd>confirm q<cr>", "Quit" },
+    -- NvimTree
+    ["<Leader>f"] = {
       function()
-        require("lazy").install()
-      end,
-      desc = "Install Plugins",
-    },
-    {
-      "<Leader>ph",
-      function()
-        require("lazy").home()
-      end,
-      desc = "Home Page",
-    },
-    {
-      "<Leader>ps",
-      function()
-        require("lazy").sync()
-      end,
-      desc = "Sync Plugins",
-    },
-    {
-      "<Leader>pc",
-      function()
-        require("lazy").check()
-      end,
-      desc = "Check Updates",
-    },
-    {
-      "<Leader>pu",
-      function()
-        require("lazy").update()
-      end,
-      desc = "Update Plugins",
-    },
-    -- Nvim Tree
-    {
-      "<Leader>f",
-      function()
-        local api = require("nvim-tree.api")
-        local currentBuf = vim.api.nvim_get_current_buf()
-        local currentBufFt = vim.api.nvim_get_option_value("filetype", { buf = currentBuf })
-        if currentBufFt == "NvimTree" then
-          api.tree.toggle()
+        local currentBuf = vim.api.nvim_get_option_value("filetype", { buf = vim.api.nvim_get_current_buf() })
+        if currentBuf == "NvimTree" then
+          require("nvim-tree.api").tree.toggle()
         else
-          api.tree.focus()
+          require("nvim-tree.api").tree.focus()
         end
       end,
-      desc = "Open File Explorer",
+      "Nvim Tree"
     },
+    -- Lazy
+    ["<Leader>p"] = { "", "+Lazy" },
+    ["<Leader>pi"] = { function() require("lazy").install() end, "Install Plugins" },
+    ["<Leader>ph"] = { function() require("lazy").home() end, "Home Page" },
+    ["<Leader>ps"] = { function() require("lazy").sync() end, "Sync Plugins" },
+    ["<Leader>pc"] = { function() require("lazy").check() end, "Check For Updates" },
+    ["<Leader>pu"] = { function() require("lazy").update() end, "Update Plugins" },
     -- Telescope
-    { "<Leader>j", group = "+File" },
-    {
-      "<Leader>jf",
-      function()
-        require("telescope.builtin").fd()
-      end,
-      desc = "Find Files",
-    },
-    {
-      "<Leader>jc",
-      function()
-        require("telescope.builtin").current_buffer_fuzzy_find()
-      end,
-      desc = "Find Files in Current Buffer",
-    },
-    {
-      "<Leader>jg",
-      function()
-        require("telescope.builtin").git_files()
-      end,
-      desc = "Find Files Tracked by Git",
-    },
-    {
-      "<Leader>jm",
-      function()
-        require("telescope.builtin").git_commits()
-      end,
-      desc = "List Commits for Current Directory",
-    },
-    {
-      "<Leader>jr",
-      function()
-        require("telescope.builtin").oldfiles()
-      end,
-      desc = "Open Recent Files",
-    },
+    ["<Leader>t"] = { "", "+Telescope" },
+    ["<Leader>tf"] = { function() require("telescope.builtin").fd() end, "Find Files" },
+    ["<Leader>tc"] = { function() require("telescope.builtin").current_buffer_fuzzy_find() end, "Find Files in Current Buffer" },
+    ["<Leader>tg"] = { function() require("telescope.builtin").git_files() end, "Find Files Tracked by Git" },
+    ["<Leader>tm"] = { function() require("telescope.builtin").git_commits() end, "List Commits for Current Directory" },
+    ["<Leader>tr"] = { function() require("telescope.builtin").oldfiles() end, "Open Recent Files" },
     -- BufDelete
-    {
-      "<Leader>k",
-      function()
-        require("bufdelete").bufdelete(0, true)
-      end,
-      desc = "Delete Current Buffer",
-    },
+    ["<Leader>k"] = { function() require("bufdelete").bufdelete(0, true) end, "Delete Current Buffer" },
     -- Toggleterm
-    {
-      "<A-t>",
-      function()
-        require("toggleterm").toggle()
-      end,
-      desc = "Toggle Terminal",
-    },
+    ["<A-t>"] = { function() require("toggleterm").toggle() end, "Toggle Integrated Terminal" },
     -- Barbar
-    {
-      "<A-,>",
-      function()
-        require("barbar.api").goto_buffer_relative(-1)
-      end,
-      desc = "Go to Previous Tab",
-    },
-    {
-      "<A-.>",
-      function()
-        require("barbar.api").goto_buffer_relative(1)
-      end,
-      desc = "Go to Next Tab",
-    },
-    {
-      "<Leader>b",
-      group = "+Buffers",
-    },
-    {
-      "<Leader>bc",
-      function()
-        require("barbar.api").close_all_but_current()
-      end,
-      desc = "Close All Tabs Except Current",
-    },
-    {
-      "<Leader>br",
-      function()
-        require("barbar.api").restore_buffer()
-      end,
-      desc = "Restore Tab",
-    },
-    {
-      "<Leader>bg",
-      function(ind)
-        require("barbar.api").goto_buffer(ind)
-      end,
-      desc = "Go To Tab",
-    },
-    {
-      "<Leader>bp",
-      function(num)
-        require("barbar.api").toggle_pin(num)
-      end,
-      desc = "Toggle Pin on Tab",
-    },
+    ["<Leader>b"] = { "", "+Barbar" },
+    ["<A-,>"] = { function() require("barbar.api").goto_buffer_relative(-1) end, "Go to Previous Tab" },
+    ["<A-.>"] = { function() require("barbar.api").goto_buffer_relative(1) end, "Go to Next Tab" },
+    ["<Leader>bc"] = { function() require("barbar.api").close_all_but_current() end, "Close All Tabs Except Current" },
+    ["<Leader>br"] = { function() require("barbar.api").restore_buffer() end, "Restore Tab" },
+    ["<Leader>bg"] = { function(ind) require("barbar.api").goto_buffer(ind) end, "Go To Tab (ind)" },
     -- Gitsigns
-    { "<Leader>g", group = "+Gitsigns" },
-    {
-      "<Leader>gt",
-      function()
-        require("gitsigns").toggle_signs()
-      end,
-      desc = "Toggle signs",
-    },
-    {
-      "<Leader>gd",
-      function()
-        require("gitsigns").toggle_deleted()
-      end,
-      desc = "Toggle Deleted",
-    },
-    {
-      "<Leader>gf",
-      function()
-        require("gitsigns").diffthis()
-      end,
-      desc = "Git Diff",
-    },
+    ["<Leader>g"] = { "", "+Gitsigns" },
+    ["<Leader>gt"] = { function() require("gitsigns").toggle_signs() end, "Toggle Gitsigns" },
+    ["<Leader>gd"] = { function() require("gitsigns").toggle_deleted() end, "Toggle Deleted" },
+    ["<Leader>gf"] = { function() require("gitsigns").diffthis() end, "Git Diff" },
   },
   t = {
-    {
-      "<A-t>",
-      function()
-        require("toggleterm").toggle()
-      end,
-      desc = "Toggle Terminal",
-    },
-  },
+    ["<A-t>"] = { function() require("toggleterm").toggle() end, "Toggle Integrated Terminal" },
+  }
 }
 
-
-local ok, wk = pcall(require, "which-key")
-
-if ok then
-  for mode, mapping in pairs(mapping_table) do
-    wk.add({ mapping, mode = mode })
+for mode, tbl in pairs(mappings) do
+  for key, mapping in pairs(tbl) do
+    vim.keymap.set(mode, key, mapping[1], { desc = mapping[2] })
   end
 end
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,73 +1,116 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"saadparwaiz1/cmp_luasnip",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-nvim-lsp",
-		"L3MON4D3/LuaSnip",
-	},
-	event = "InsertEnter",
-	opts = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+  },
+  event = "InsertEnter",
+  opts = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
 
-		local function has_words_before()
-			local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
+    local function has_words_before()
+      local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
 
-		-- Settings below taken from Astronvim
-		return {
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = require("cmp").config.window.bordered(),
-				documentation = require("cmp").config.window.bordered(),
-			},
-			mapping = {
-				["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-				["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-				["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-				["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-				["<C-y>"] = cmp.config.disable,
-				["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			},
-			sources = {
-				{ name = "nvim_lsp", priority = 1000 },
-				{ name = "luasnip", priority = 750 },
-				{ name = "buffer", priority = 500 },
-				{ name = "path", priority = 250 },
-			},
-		}
-	end,
+    -- Settings below taken from Astronvim
+    return {
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = require("cmp").config.window.bordered(),
+        documentation = require("cmp").config.window.bordered(),
+      },
+      mapping = {
+        ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+        ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+        ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+        ["<C-y>"] = cmp.config.disable,
+        ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      },
+      sources = {
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "luasnip",  priority = 750 },
+        { name = "buffer",   priority = 500 },
+        { name = "path",     priority = 250 },
+      },
+    }
+  end,
 }
+-- return {
+--   "saghen/blink.cmp",
+--   lazy = false,
+--   dependencies = { "rafamadriz/friendly-snippets" },
+--   build = "cargo build --release",
+--   opts = {
+--     highlight = {
+--       use_nvim_cmp_as_default = true,
+--     },
+--     keymap = {
+--       accept = "<CR>",
+--       select_prev = { "<C-k>" },
+--       select_next = { "<C-j>" },
+--     },
+--     accept = { auto_brackets = { enabled = true } },
+--     sources = {
+--       providers = {
+--         { "blink.cmp.sources.lsp",    name = "lsp" },
+--         {
+--           "blink.cmp.sources.snippets",
+--           name = "snippets",
+--           score_offset = -3,
+--           opts = {
+--             extended_filetypes = {
+--               search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+--               extended_filetypes = {
+--                 c = { "cpp" },
+--               },
+--             },
+--           },
+--         },
+--         { "blink.cmp.sources.buffer", name = "buffer", score_offset = -2 },
+--         { "blink.cmp.sources.path",   name = "path" },
+--       },
+--     },
+--     windows = {
+--       autocomplete = {
+--         selection = "manual",
+--         draw = "reversed",
+--       },
+--     },
+--   },
+-- }
